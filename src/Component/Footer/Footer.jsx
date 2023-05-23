809e0565d8b574d9906d4801379ef01bd73e038c
thuyhoang11 <134189922+thuyhoang11@users.noreply.github.com>
2023-05-23 17:07:56 +0700

    Update Footer.jsx

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div style={{width: "100%"}}>
-      <img draggable={false} style={{width: "100%"}} src="https://res.cloudinary.com/cockbook/image/upload/v1677413994/single/Screenshot_2023-02-26_191940_dse15p.png" alt="" />
+      <img draggable={false} style={{width: "100%"}} src="https://res.cloudinary.com/dmkk2kmyd/image/upload/v1684833046/Footer-Final_abimlv.png" alt="" />
     </div>
   )
 }
