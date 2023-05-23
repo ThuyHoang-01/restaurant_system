c47b0fa7f5ec5ce212ecb91d7b7f2d41b1947801
Hoàng Thị Tố Thùy <httthuy455@gmail.com>
2023-05-24 01:19:53 +0700

    commit

--- src/Component/Admin/components/widgetLg/WidgetLg.jsx
+++ src/Component/Admin/components/widgetLg/WidgetLg.jsx
@@ -14,6 +14,7 @@ export default function WidgetLg() {
           <th className="widgetLgTh">Số lượng</th>
           <th className="widgetLgTh">Trạng thái</th>
         </tr>
+        
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
@@ -21,40 +22,10 @@ export default function WidgetLg() {
               alt=""
               className="widgetLgImg"
             />
-            <span className="widgetLgName">Susan Carol</span>
+            <span className="widgetLgName">Hồ Hồng Thủy</span>
           </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Declined" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgDate">24 May 2023</td>
+          <td className="widgetLgAmount">2.000.000 VNĐ</td>
           <td className="widgetLgStatus">
             <Button type="Pending" />
           </td>
@@ -68,8 +39,8 @@ export default function WidgetLg() {
             />
             <span className="widgetLgName">Susan Carol</span>
           </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgDate">2 May 2023</td>
+          <td className="widgetLgAmount">2.000.000 VNĐ</td>
           <td className="widgetLgStatus">
             <Button type="Approved" />
           </td>
